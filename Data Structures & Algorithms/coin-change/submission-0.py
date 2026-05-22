acbc9d86bc4fe61d41b8325be8a4309f8e440fb9
Daniel Ting <oldtim346@gmail.com>
2026-05-22 15:20:22 +0800

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        coins.sort()  # 因為coins最多只有10種，logN << t (最高10000)
+
+        dp = [-1]*(amount+1) #
+        dp[0] = 0
+        for i in range(len(dp)):
+            for j in range(len(coins)):
+                if dp[i] >= 0 and (i + coins[j]) <= amount:
+                    if dp[(i + coins[j])] < 0:
+                        dp[(i + coins[j])] = dp[i] + 1
+                    else:
+                        dp[(i + coins[j])] = min(dp[(i + coins[j])], dp[i]+1)
+        
+        return dp[-1]
+
+
+
+
+
+
+        #瞄一眼解答: 轉換思路，因為coins是整數，可把每一點amount當作dp格子，每格紀錄累積到此量所需最小次數
